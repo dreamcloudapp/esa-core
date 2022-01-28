1b6f7b56b2033f958152300c33f9850b414f2dd3
Sheldon Juncker <sheldonjuncker@gmail.com>
2022-01-28 18:58:45 -0500

    Making methods public.

--- src/main/java/com/dreamcloud/esa_core/similarity/DocumentSimilarity.java
+++ src/main/java/com/dreamcloud/esa_core/similarity/DocumentSimilarity.java
@@ -10,11 +10,11 @@ public class DocumentSimilarity {
         this.vectorizer = vectorizer;
     }
 
-    private SimilarityInfo score(String doc1, String doc2) throws Exception {
+    public SimilarityInfo score(String doc1, String doc2) throws Exception {
         return score(doc1, doc2, false);
     }
 
-    private SimilarityInfo score(String doc1, String doc2, boolean gatherTopConcepts) throws Exception {
+    public SimilarityInfo score(String doc1, String doc2, boolean gatherTopConcepts) throws Exception {
         DocumentScoreVector doc1Vector = vectorizer.vectorize(doc1);
         DocumentScoreVector doc2Vector = vectorizer.vectorize(doc2);
         return doc1Vector.dotProduct(doc2Vector, gatherTopConcepts);
